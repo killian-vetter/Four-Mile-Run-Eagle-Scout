5275228f87e392c870ad497740132ec47f941897
killv5 <killianvetter@gmail.com>
2019-06-28 17:20:37 -0400

    Finished login.py

--- Lambda/login.py
+++ Lambda/login.py
@@ -1,1 +1,52 @@
-float(response['Item']['Minutes']['N'])
+import boto3
+import os
+import random
+
+client = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    response = {}
+    noPics = os.environ['NoPics'].split(",")
+    item = client.get_item(
+        TableName='Man-Hours',
+        Key={
+            'Name': {'S' : event['name']}
+        })
+    try:
+        picNo = int(item['Item']['PicNo']['N'])
+        cam = int(item['Item']['Cam']['N'])
+        if(picNo > int(noPics[cam-1])):
+            picNo = 1
+    except:
+        cam = 1
+        #cam = random.randrange(1,3)
+        picNo = random.randrange(1, int(noPics[cam-1]))
+        client.put_item(
+            TableName = 'Man-Hours',
+            Item = {
+                "Name" : {
+                    "S" : event['name']
+                },
+                "Minutes" : {
+                    "N" : "0"
+                },
+                "Cam" : {
+                    "N" : str(cam)
+                },
+                "PicNo" : {
+                    "N" : str(picNo)
+                }
+            }
+        )
+    finally:
+        response['image'] = str(cam) + "/" + picName(picNo, len(str(picNo)))
+        return response
+
+# creates the name of the picture given its identifying number
+def picName(num, length):
+    name = "IMG_"
+    for i in range(length, 4):
+        if (i != 4):
+            name += "0"
+    name += str(num) + ".JPG"
+    return name
