c91940494d9f82466ec234605637365c099a0761
killv5 <killianvetter@gmail.com>
2019-10-06 00:38:09 -0400

    first draft of file upload function

--- Lambda/renameFiles.py
+++ Lambda/renameFiles.py
@@ -5,29 +5,29 @@ create a thumbnail for the image (it will automatically load with the full loadi
 '''
 
 import boto3
+from PIL import Image
+import re
 
 client = boto3.client('s3')
 
-def picName(num, length):
-    name = "IMG_"
-    for i in range(length, 4):
-        if (i != 4):
-            name += "0"
-    name += str(num) + ".JPG"
-    return name
+def createName(image):
+    #use https://www.awaresystems.be/imaging/tiff/tifftags/privateifd/exif.html
+    exif = image._getexif()
+    date = exif[36867]
+    comment = exif[37510]
+    cam = comment[21:27]
+    return date.replace(" ","-")+can+'.JPG'
 
-def lambda_handler(event, context): 
-    num = int(client.get_object(
-        Bucket = "four-mile-run",
-        Key = "num.txt")['Body'].read().decode('utf-8'))
-
+def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = unquote_plus(record['s3']['object']['key'])
-        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-        upload_path = '/img/'+ picName(num, len(num))
-        client.download_file(bucket, key, download_path)
-        client.upload_file(upload_path, "four-mile-run", picName(num, len(num)))
-        num = num + 1
-
+        obj = Image.read(client.get_record(Bucket=bucket, Key=key))
+        # resize picture and save in two different folders img/full and img/thumbnail
+        name = createName(obj)
+        client.put_object(Bucket='four-mile-run', Key='img/full/'+name, Body=obj)
+        obj.thumbnail((1280,720))
+        client.put_object(Bucket='four-mile-run', Key='img/thumbnail/'+name, Body=obj)
+        client.delete_object(Bucket=bucket, Key=key)
+        
     client.download_filobj("four-mile-run", "num.txt", str(num).encode())
