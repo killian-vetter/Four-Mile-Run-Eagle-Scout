1871275f1c6a10cee2c010e54c2752bcfcc7f2d0
Killian <killian@localhost.localdomain>
2019-11-06 17:36:28 -0500

    Finished renameFiles function

--- Lambda/renameFiles/lambda_function.py
+++ Lambda/renameFiles/lambda_function.py
@@ -5,6 +5,7 @@ create a thumbnail for the image (it will automatically load with the full loadi
 '''
 
 import boto3
+import uuid
 from PIL import Image
 from urllib.parse import unquote_plus
 
@@ -15,17 +16,20 @@ def createName(image):
     exif = image._getexif()
     date = exif[36867]
     comment = exif[37510]
-    cam = comment[21:27]
+    cam = comment[21:27].decode("utf-8")
     return cam+date.replace(" ","-")+'.JPG'
 
 def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = unquote_plus(record['s3']['object']['key'])
-        obj = Image.open(client.get_object(Bucket=bucket, Key=key)['Body'].read())
-        # resize picture and save in two different folders img/full and img/thumbnail
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+        client.download_file(bucket, key, download_path)
+        obj = Image.open(download_path)
         name = createName(obj)
-        client.put_object(Bucket='four-mile-run', Key='img/full/'+name, Body=obj)
+        resize_path = '/tmp/{}'.format(name)
+        client.upload_file(download_path, 'four-mile-run', 'img/full/'+name)
         obj.thumbnail((1280,720))
-        client.put_object(Bucket='four-mile-run', Key='img/thumbnail/'+name, Body=obj)
+        obj.save(resize_path)
+        client.upload_file(resize_path, 'four-mile-run', 'img/thumbnail/'+name)
         client.delete_object(Bucket=bucket, Key=key)
